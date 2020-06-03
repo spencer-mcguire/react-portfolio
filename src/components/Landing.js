6234a1842a8295282862fd9c3e34f25ec5e9301c
spencer-mcguire <smcguire1826@gmail.com>
2020-06-03 13:01:13 -0500

    basic components

---
+++ src/components/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Landing = () => {
+  return (
+    <div>
+      <p>This is the landing page</p>
+      <p> FLOATING DOWN ARROW WILL BE HERE</p>
+    </div>
+  );
+};
