5ff8c1cc2652488eb6fe533ebf34a46b6b569fe8
spencer-mcguire <smcguire1826@gmail.com>
2020-06-03 20:50:01 -0500

    add icons

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { BsChevronCompactDown } from 'react-icons/bs';
+import { Flex } from '@chakra-ui/core';
+
 export const Landing = () => {
   return (
-    <div>
-      <p>This is the landing page</p>
-      <p> FLOATING DOWN ARROW WILL BE HERE</p>
+    <div className='Landing'>
+      {/*Main container  */}
+      <Flex justify='center' w='100%' h='100%' alignContent='center'>
+        <Flex flexDir='column' align='center' justify='center'>
+          <p>This is the landing page</p>
+
+          <BsChevronCompactDown />
+        </Flex>
+      </Flex>
     </div>
   );
 };
