16b9679f8165abd3160c56c74ddb3adcf969712a
spencer-mcguire <smcguire1826@gmail.com>
2020-08-03 11:24:14 -0500

    change landing layout starter

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import { BsChevronCompactDown } from 'react-icons/bs';
-import { Flex, Box } from '@chakra-ui/core';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { Flex, Box, Text } from '@chakra-ui/core';
 
 export const Landing = () => {
   // function to scroll to about me section
@@ -14,19 +15,41 @@ export const Landing = () => {
   return (
     <div className='Landing'>
       {/*Main container  */}
-      <Flex justify='center' w='100%' minH='100vh' alignContent='center'>
-        <Flex flexDir='column' align='center' justify='center'>
-          <Flex h='50%' align='flex-end'>
-            <h1>Spencer McGuire</h1>
+      <Flex w='100%' h='100%' minH='100vh' flexDir='column'>
+        {/* TOP CONTAINER */}
+        <Flex w='100%' h='60%' border='2px solid red'>
+          <Flex
+            w='50%'
+            align='center'
+            justify='center'
+            border='2px solid green'
+            flexDir='column'
+          >
+            <Text fontSize={50}>
+              I'm a Full Stack <br></br> Web Developer
+            </Text>
+            {/* SOCIAL CONTAINER */}
+            <Flex>
+              <Box as={AiFillGithub} size='32px' color='grey' />
+              <Box as={AiFillLinkedin} size='32px' color='grey' />
+            </Flex>
           </Flex>
+        </Flex>
+        {/* BOTTOM CONTAINER */}
+        <Flex w='100%' h='40%' border='2px solid blue' background='#ff2252'>
+          <p> stuff</p>
+        </Flex>
+        {/* <Flex flexDir='column' align='center' justify='center'>
+          <h1> Spencer McGuire </h1>
+          <h3> Full Stack Developer </h3>
           <Flex h='50%' align='flex-end' pb='39%'>
             <Box
               as={BsChevronCompactDown}
               size='90px'
               onClick={scrollToAbout}
             />
-          </Flex>
-        </Flex>
+          </Flex> */}
+        {/* </Flex> */}
       </Flex>
     </div>
   );
