3936a4653707ff78ab3161e81d27868c80ad26c0
spencer-mcguire <smcguire1826@gmail.com>
2020-08-04 18:53:41 -0500

    Add CSS for flipping cards (not used)

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 import DevImg from '../Developer activity-bro.svg';
-import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin, AiOutlineComment } from 'react-icons/ai';
+import { BsCodeSlash } from 'react-icons/bs';
+import { IoIosBarcode } from 'react-icons/all';
 import { Flex, Box, Text, Button, Link, Image } from '@chakra-ui/core';
 
 export const Landing = () => {
@@ -65,29 +67,81 @@ export const Landing = () => {
         {/* BOTTOM CONTAINER */}
         <Flex
           w='100%'
-          h='40%'
+          h='35%'
+          mb='5%'
           background='#ff2252'
           justify='center'
           align='center'
+          flexDir='column'
         >
+          <Flex
+            w='100%'
+            h='70%'
+            maxW='1000px'
+            padding='2%'
+            align='center'
+            justify='center'
+            flexDir='column'
+          >
+            <Text fontSize='5xl' color='white' mb='30px'>
+              Hey, I'm Spencer. Nice to meet you.
+            </Text>
+            <Text fontSize='lg' color='white' textAlign='center'>
+              He's like the rest of us -- he's about half crazy. It takes a
+              very, very little bit. Don't be afraid to go out on a limb once in
+              a while. You learn to take advantage of whatever happens and use
+              it to make your painting special. Don't over do it -- be careful.
+              Be careful.
+            </Text>
+          </Flex>
           {/* CARD CONTAINER */}
           <Flex
             maxW='1920px'
             w='100%'
-            h='100%'
-            border='2px solid blue'
-            justify='space-around'
-            align='center'
+            h='30%'
+            justify='center'
+            align='flex-start'
           >
-            <Box h='60%' w='10%' bg='white'>
-              <p>Content</p>
-            </Box>
-            <Box h='10%' w='10%' bg='white'>
-              <p>Content</p>
-            </Box>
-            <Box h='10%' w='10%' bg='white'>
-              <p>Content</p>
-            </Box>
+            <Flex
+              h='1000px'
+              w='20%'
+              p='1%'
+              zIndex='11'
+              bg='white'
+              border='1px solid grey'
+              roundedLeft='20px'
+              flexDir='column'
+              align='center'
+            >
+              <Box as={AiOutlineComment} size='100px' mb='30px' mt='10px' />
+              <h2>Front-end Developer</h2>
+            </Flex>
+
+            <Flex
+              h='1000px'
+              w='20%'
+              p='1%'
+              bg='grey'
+              borderY='1px solid grey'
+              flexDir='column'
+              align='center'
+            >
+              <Box as={BsCodeSlash} size='100px' mb='30px' mt='10px' />
+              <h2>Back-end Developer</h2>
+            </Flex>
+            <Flex
+              h='1000px'
+              w='20%'
+              p='1%'
+              bg='white'
+              border='1px solid grey'
+              roundedRight='20px'
+              flexDir='column'
+              align='center'
+            >
+              <Box as={IoIosBarcode} size='100px' mb='30px' mt='10px' />
+              <h2>What makes me different?</h2>
+            </Flex>
           </Flex>
         </Flex>
       </Flex>
